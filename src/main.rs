8075ba24ead1ca420053d9df23e533b48fb49f29
solmann <DanGSun@yandex.ru>
2020-02-13 20:05:27 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,124 @@
+mod universe;
+
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events, EventLoop};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent, ButtonEvent, PressEvent, ReleaseEvent};
+use piston::window::WindowSettings;
+use graphics::*;
+use universe::Universe;
+use crate::universe::Body;
+use nalgebra::{Point2, Vector2, Matrix2, Matrix2x1};
+use graphics::ellipse::circle;
+
+pub struct App {
+    gl: GlGraphics,
+    universe: Universe,
+    old_pos: Vec<(f64, f64)>,
+    pub cam_x: f64,
+    pub cam_y: f64
+}
+
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+
+
+
+        let im = self.universe.bodies.iter();
+        let cam = (self.cam_x.clone(), self.cam_y.clone());
+        self.gl.clear_color(WHITE);
+        self.gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform;
+            for i in im {
+                if i.mass < 10000. {
+                    if i.charge < 0. {
+                        ellipse(GREEN, circle(i.loc.x+cam.0, i.loc.y+cam.1,i.mass/900.), transform, gl);
+                    } else {
+                        ellipse(RED, circle(i.loc.x+cam.0, i.loc.y+cam.1,i.mass/900.), transform, gl);
+                    }
+                } else {
+                    ellipse(BLACK, circle(i.loc.x+cam.0, i.loc.y+cam.1,i.mass/1500.), transform, gl);
+                }
+            }
+        });
+
+    }
+
+    fn update(&mut self, args: &UpdateArgs) {
+        self.universe.tick();
+    }
+}
+
+
+fn main() {
+    // Change this to OpenGL::V2_1 if not working.
+    let opengl = OpenGL::V3_2;
+
+    // Create an Glutin window.
+    let mut window: Window = WindowSettings::new("Orbit-Sim", [1920, 1080])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    // Create a new sim and run it.
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        universe: Universe::new(),
+        old_pos: vec![],
+        cam_x: 0.,
+        cam_y: 0.
+    };
+
+    // Setting up some testing particles
+
+    // At.1
+    let pos = Body::new(500., 500., 20000., 2e12);
+    let mut neg = Body::new(540., 500., 8000., -2e12);
+    let mut neg2 = Body::new(460., 500., 8000., -2e12);
+
+    neg.force += Vector2::new(0., 0.05);
+    neg2.force += Vector2::new(0., -0.05);
+
+    app.universe.add_body(pos);
+    app.universe.add_body(neg);
+    app.universe.add_body(neg2);
+
+    // At.2
+    let pos2 = Body::new(1600., 500., 20000., 2e12);
+    let mut neg3 = Body::new(1640., 500., 8000., -2e12);
+    let mut neg4 = Body::new(1560., 500., 8000., -2e12);
+
+    neg3.force += Vector2::new(0., 0.05);
+    neg4.force += Vector2::new(0., -0.05);
+
+    app.universe.add_body(pos2);
+    app.universe.add_body(neg3);
+    app.universe.add_body(neg4);
+
+
+    let mut event_sets = EventSettings::new();
+    event_sets.set_ups(10000);
+
+    let mut events = Events::new(event_sets);
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            app.render(&args);
+        }
+
+        if let Some(args) = e.update_args() {
+            app.update(&args);
+            // break
+        }
+    }
+}
---
+++ src/universe.rs
@@ -0,0 +1,87 @@
+extern crate nalgebra as na;
+use std::time::Duration;
+use std::thread;
+use std::f64::consts::PI;
+use na::{Vector2, Point2};
+use std::thread::sleep;
+
+const G: f64 = 6.67430e-11;
+const E0: f64 = 8.85418e-12;
+const B_CHARGE: f64 = 1.60217e-19;
+
+#[derive(Clone)]
+pub struct Body {
+    pub loc: Point2<f64>,
+    pub force: Vector2<f64>,
+    pub mass: f64,
+    pub charge: f64
+    // z: f64,
+    // color: (u8, u8, u8)
+}
+
+impl Body {
+    pub fn origin() -> Body {
+        Body { loc: Point2::origin(), mass: 0.0, force: Vector2::new(0., 0.), charge: 0. }
+    }
+
+    pub fn new(x: f64, y: f64, mass: f64, charge: f64) -> Body {
+        Body { loc: Point2::new(x, y), mass, force: Vector2::new(0., 0.), charge: charge*B_CHARGE }
+    }
+
+    pub fn update_loc(&mut self) {
+        self.loc = &self.loc + &self.force
+    }
+}
+
+pub struct Universe {
+    pub bodies: Vec<Body>,
+    time_since_beginning: u64,
+}
+
+pub fn gravity(a_body: &Body, b_body: &Body) -> Vector2<f64> {
+    let mass_a = a_body.mass.clone();
+    let mass_b = b_body.mass.clone();
+    let loc_a = a_body.loc.clone();
+    let loc_b = b_body.loc.clone();
+
+    let gt_force = (G * mass_a * mass_b) / (loc_b - loc_a).norm_squared();
+    return gt_force*(loc_b - loc_a);
+}
+
+pub fn electrostatic(a_body: &Body, b_body: &Body) -> Vector2<f64> {
+    let q_a = a_body.charge.clone();
+    let q_b = b_body.charge.clone();
+    let loc_a = a_body.loc.clone();
+    let loc_b = b_body.loc.clone();
+    let result = ((1./(4.*PI*E0))*((q_a*q_b)/(loc_a - loc_b).norm_squared()))*(loc_a-loc_b);
+    // println!("{}", result);
+    result
+}
+
+impl Universe {
+    pub fn new() -> Universe {
+        Universe { bodies: vec![], time_since_beginning: 0 }
+    }
+
+    pub fn tick(&mut self) {
+        self.time_since_beginning += 1;
+        // Common forces
+        for i in 0..self.bodies.len() {
+            for j in 0..self.bodies.len() {
+                if i == j { continue };
+                let mut fin_force = self.bodies[i].force.clone();
+                fin_force += gravity(&self.bodies[i], &self.bodies[j]);
+                fin_force += electrostatic(&self.bodies[i], &self.bodies[j]);
+                self.bodies[i].force = fin_force;
+            }
+        }
+        for i in self.bodies.iter_mut() {
+            i.update_loc();
+        };
+        // sleep(Duration::from_secs_f64(0.1));
+    }
+
+    pub fn add_body(&mut self, body: Body) {
+        self.bodies.push(body)
+    }
+}
